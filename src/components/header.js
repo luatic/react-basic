36ce5de1d7e82486201a5fd95f6fe94033bfaf93
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-01-13 23:33:55 +0000

    cyk

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Header = () => {
-  return <div>dupa header</div>
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">Logo</div>
+        <input type="text" />
+      </header>
+    )
+  }
 }
 
 export default Header
