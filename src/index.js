c18afebd8565deb16ad12bc24eecf98ef86ab6ad
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-01-13 03:06:26 +0000

    addd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+
+const App = () => {
+  return <h1>jak tam </h1>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
