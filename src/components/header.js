a96cff53f76a259505cd8a93b33ac61aa4adb4f5
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-01-15 21:58:12 +0000

    cccdd

--- src/components/header.js
+++ src/components/header.js
@@ -2,14 +2,13 @@ import React, {Component} from 'react'
 import '../css/style.css'
 
 class Header extends Component {
+  input() {
+    console.log('cos tam')
+  }
   render() {
     return (
       <header>
-        <div
-          className="logo"
-          onClick={() => {
-            console.log('klik')
-          }}>
+        <div className="logo" onClick={this.input}>
           Logo
         </div>
         <input type="text" />
