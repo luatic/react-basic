9ce7ff27de2fb23402451cc6d186cc90ac6807a7
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-01-18 22:15:36 +0000

    cyk

--- src/components/header.js
+++ src/components/header.js
@@ -7,16 +7,19 @@ body{
 	margin: 0px 0px 0px 0px;
 }
 `
-const Logo = Styled.h1` color: #FFA500; font-size: 40px;
+const Logo = Styled.h1` 
+	color: #FFA500; 
+	font-size: 40px;
 	font-family: 'Anton';
 `
 const Wrapper = Styled.header`
   background: #03a9f4;
 	text-align: center;
+	padding-top: 10px;
 `
 const Input = Styled.input`
-  font-size: 20px;
-	margin: 20px 0;
+	font-size: 25px;
+	margin: 25px 0px 75px 0px;
 `
 class Header extends Component {
   inputChangeHandler(e) {
@@ -27,7 +30,7 @@ class Header extends Component {
     return (
       <Wrapper>
         <GlobalStyle />
-        <Logo className="logo"> Logo</Logo>
+        <Logo> Logo</Logo>
         <Input type="text" onChange={this.inputChangeHandler} />
       </Wrapper>
     )
