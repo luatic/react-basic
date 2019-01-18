3f569e77553aa20fc7fae1d0f840f1da72d372c3
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-01-18 00:22:52 +0000

    added styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react'
-import '../css/style.css'
+import Styled from 'styled-components'
+import {createGlobalStyle} from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+body{
+	margin: 0px 0px 0px 0px;
+}
+`
+const Logo = Styled.h1`
+  color: #FFA500;
+	font-size: 40px;
+	font-family: 'Anton';
+`
+const Wrapper = Styled.header`
+  background: #03a9f4;
+	text-align: center;
+`
+const Input = Styled.input`
+  font-size: 20px;
+	margin: 20px 0;
+`
 class Header extends Component {
+  inputChangeHandler(e) {
+    console.log(e.target.value)
+  }
+
   render() {
     return (
-      <header>
-        <div
-          className="logo"
-          onClick={() => {
-            console.log('klik')
-          }}>
-          Logo
-        </div>
-        <input type="text" />
-      </header>
+      <Wrapper>
+        <GlobalStyle />
+        <Logo className="logo"> Logo</Logo>
+        <Input type="text" onChange={this.inputChangeHandler} />
+      </Wrapper>
     )
   }
 }
