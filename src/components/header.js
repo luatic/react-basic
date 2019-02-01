b520b6a0e908d373badee4e898c71eb5a3c39e78
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-02-01 19:44:14 +0000

    cos

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,37 @@
 import React, {Component} from 'react'
-import Styled from 'styled-components'
-import {createGlobalStyle} from 'styled-components'
+import styled, {createGlobalStyle} from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 body{
 	margin: 0px 0px 0px 0px;
 }
 `
-const Logo = Styled.h1` 
-	color: #FFA500; 
-	font-size: 40px;
-	font-family: 'Anton';
+const Logo = styled.h1`
+  color: #ffa500;
+  font-size: 40px;
+  font-family: 'Anton';
 `
-const Wrapper = Styled.header`
+const Wrapper = styled.header`
   background: #03a9f4;
-	text-align: center;
-	padding-top: 10px;
+  text-align: center;
+  padding-top: 10px;
 `
-const Input = Styled.input`
-	font-size: 25px;
-	margin: 25px 0px 75px 0px;
+const Input = styled.input`
+  font-size: 25px;
+  margin: 25px 0px 75px 0px;
 `
 class Header extends Component {
-  inputChangeHandler(event) {
-    console.log(event.target.value)
+  state = {
+    keywords: 'witaj',
+  }
+  inputChangeHandler = e => {
+    this.setState({
+      keywords: e.target.value,
+    })
   }
 
   render() {
+    console.log(this.state.keywords)
     return (
       <Wrapper>
         <GlobalStyle />
