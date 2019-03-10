62e2a37695798ed5f465e419a79a404dfd879e4e
Patrycjusz Myszka <zavulon1981@gmail.com>
2019-03-10 16:50:14 +0000

    npm update

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,7 @@
 import React, {Component} from 'react'
-import styled, {createGlobalStyle} from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-body{
-	margin: 0px 0px 0px 0px;
-}
-`
-const Logo = styled.h1`
-  color: #ffa500;
-  font-size: 40px;
-  font-family: 'Anton';
-`
-const Wrapper = styled.header`
-  background: #03a9f4;
-  text-align: center;
-  padding-top: 10px;
-`
-const Input = styled.input`
-  font-size: 25px;
-  margin: 25px 0px 75px 0px;
-`
-class Header extends Component {
-  state = {
-    keywords: 'witaj',
-  }
+export default class Header extends Component {
+  state = {}
 
   inputChangeHandler = e => {
     this.setState({
@@ -31,15 +9,6 @@ class Header extends Component {
     })
   }
   render() {
-    console.log(this.state.keywords)
-    return (
-      <Wrapper>
-        <GlobalStyle />
-        <Logo> Logo</Logo>
-        <Input type="text" onChange={this.inputChangeHandler} />
-      </Wrapper>
-    )
+    return <div>cc</div>
   }
 }
-
-export default Header
